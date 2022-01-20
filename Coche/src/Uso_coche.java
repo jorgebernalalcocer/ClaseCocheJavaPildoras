65a17228d44fd16632e2a01fda4e58b545e70f0d
jorgebernalalcocer <j.90.bernal@gmail.com>
2022-01-20 22:58:12 +0100

    terminado video 35

--- Coche/src/Uso_coche.java
+++ Coche/src/Uso_coche.java
@@ -1,46 +1,85 @@
-import javax.swing.*;
+import java.sql.Date;
+import java.util.GregorianCalendar;
+
+
+//import javax.swing.*;
 
 public class Uso_coche {
 
     public static void main(String[] args){
 
+      Empleado e1 = new Empleado("Jorge",1500,2020,4,2);
 
-        //instanciamos el objeto seat de la clase coche
+      System.out.println(e1.dameNombre());
+      System.out.println(e1.dameSueldo());
+      e1.subeSueldo(10);
+      System.out.println(e1.dameSueldo());
 
-        Coche seat = new Coche();
+      //Empleado[] misEmpleados=new Empleado[3];
 
-       
-        System.out.println(seat.dime_datos_generales());
+      Empleado[] miArray=new Empleado [3];
+      miArray[0]=new Empleado("Paco",1000, 3030, 3, 2);
+      miArray[1]=new Empleado("Juan",3000, 3030, 3, 2);
+      miArray[2]=new Empleado("manolo",2000, 3030, 3, 2);
 
-      seat.establece_climatizador(JOptionPane.showInputDialog("Lleva climatizador?"));
-      System.out.println(seat.dime_climatizador());
+      for(int i = 0; i<miArray.length; i++){
 
-        seat.establece_color(JOptionPane.showInputDialog("Introduce color"));
-        seat.establece_cv(150);
-        
-        System.out.println(seat.dime_color());
+        miArray[i].subeSueldo(10);
+      }
 
-        seat.establece_asientos_cuero(JOptionPane.showInputDialog("Lleva asientos de cuero?"));
-        
-        System.out.println(seat.dime_asientos_cuero());
+      for(int i=0; i<miArray.length;i++){
+        System.out.println("nombre: "+ miArray[i].dameNombre()+" Sueldo: "+ miArray[i].dameSueldo());
+      }
 
-        System.out.println(seat.dime_cv()+" CV");
+      
 
-        System.out.println(seat.dime_peso_coche());
-
-        System.out.println("el precio del coche es de: "+seat.precio_coche());
-
-        seat.establece_modelo("León FR");
-
-        System.out.println("el modelo es: " + seat.dime_modelo());
-
-
-
+      
   
-
-
+     
 
         }
+    }
+
+    class Empleado{
+
+      public Empleado(String nom, double sue, int agno, int mes, int dia ){
+        this.nombre=nom;
+        this.sueldo=sue;
+        GregorianCalendar altas = new GregorianCalendar(agno,mes-1,dia);
+
+        altaContrato = altas.getTime();
+
+
+      }
+
+      //GETTERS
+      public String dameNombre(){
+        return nombre;
+      }
+
+      public double dameSueldo(){
+        return sueldo;
+      }
+
+      public Date dameFechaContrato(){
+        return (Date) altaContrato;
+      }
+
+      //SETTTERS
+
+      public void subeSueldo(double porcentaje){
+        double aumento = sueldo*porcentaje/100;
+        sueldo=sueldo+aumento;
+
+      }
+
+
+
+      private String nombre;
+      private double sueldo;
+      private java.util.Date altaContrato;
+
+
     }
 
     
